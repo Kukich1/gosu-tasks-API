ed7efdd86dde202a763c3a662f41d0e9abb7f419
Куаныш <142867101+Kukich1@users.noreply.github.com>
2023-11-13 23:54:59 +0600

    Add files via upload

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -51,6 +51,34 @@ async def show_collection(username: str, current_user: str = Depends(get_current
         user_post["task_name"] = task_name['name'] if task_name else "Неизвестная задача"
         user_post["task"] = str(task_id)  # Преобразование ObjectId в строку
     
+    current_lst = user_posts + user_tasks
+    return current_lst
+
+
+@router.get("/show_completed_user_tasks&posts/{username}")
+async def show_completed_taskpost(username: str, start: int = Query(default=0, ge=0), end: int = Query(default=2000000000, le=2000000000), current_user: str = Depends(get_current_user)):
+    db = get_db()
+    posts_collection = db['posts']
+    task_collection = db['tasks']
+    project_collection = db['projects']
+    
+    user_posts = await posts_collection.find({"time_completed":{"$gte": start, "$lte": end}, "member": username, 'status': 'completed'}, {'_id': 0, 'id': 1, 'name': 1, 'description': 1, 'task': 1,'deadline': 1, 'created_at': 1, 'type': 1, 'status': 1, 'time_completed': 1, 'action': 1, 'member': 1}).to_list(length=None)
+    user_tasks = await task_collection.find({"time_completed":{"$gte": start, "$lte": end}, "members": username, 'status': 'completed'}, {'_id': 0, 'id': 1, 'name': 1, 'description': 1, 'project': 1, 'deadline': 1, 'created_at': 1, 'comments': 1, 'type': 1, 'status': 1, 'members': 1,'time_completed': 1}).to_list(length=None)
+    
+    for user_task in user_tasks:
+        project_id = user_task['project']
+        project_id = urllib.parse.unquote(project_id)
+        project_name = await project_collection.find_one({'id': project_id}, {'name': 1})
+        user_task["project_name"] = project_name['name'] if project_name else "Неизвестный проект"
+        user_task["project"] = str(project_id)
+
+    for user_post in user_posts:
+        task_id = user_post['task']
+        task_id = urllib.parse.unquote(task_id)
+        task_name = await task_collection.find_one({'id': task_id}, {'name': 1})
+        user_post["task_name"] = task_name['name'] if task_name else "Неизвестная задача"
+        user_post["task"] = str(task_id)  # Преобразование ObjectId в строку
+
     completed_lst = user_posts + user_tasks
     return completed_lst
 
